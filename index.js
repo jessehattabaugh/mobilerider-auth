df050852e512814e265ee7c2e8858bcaafacc372
Jesse Hattabaugh <arkanciscan@gmail.com>
2021-07-15 20:28:57 -0700

    create a session token

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 8080;
 
 const db = require('diskdb');
-db.connect('./db', ['users']);
+db.connect('./db', ['users', 'sessions']);
+
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,7 +14,9 @@ app.post('/signin', (req, res) => {
 	console.info(`/signin username: ${req.body.username} password: ${req.body.password}`);
 	const user = db.users.findOne({ username: req.body.username });
 	if (user.password === req.body.password) {
-		res.json({ token: '__token__' });
+		const token = uuidv4();
+		db.sessions.save({ username: user.username, token: token });
+		res.json({ token: token });
 	} else {
 		res.sendStatus(401);
 	}
