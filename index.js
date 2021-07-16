36da626b803336e9d11db44d6c2258125c03eecf
Jesse Hattabaugh <arkanciscan@gmail.com>
2021-07-15 20:21:19 -0700

    test username and password from database

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const db = require('diskdb');
+db.connect('./db', ['users']);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/signin', (req, res) => {
 	console.info(`/signin username: ${req.body.username} password: ${req.body.password}`);
-	res.json({ token: '__token__' });
+	const user = db.users.findOne({ username: req.body.username });
+	if (user.password === req.body.password) {
+		res.json({ token: '__token__' });
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 app.post('/signout', (req, res) => {
