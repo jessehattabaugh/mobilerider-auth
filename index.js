23727b0ec6a7f3f8fec607f9cf9bc3d3790289ac
Jesse Hattabaugh <arkanciscan@gmail.com>
2021-07-15 20:07:59 -0700

    spec

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/signin', (req, res) => {
+	console.info(`/signin username: ${req.body.username} password: ${req.body.password}`);
+	res.json({ token: '__token__' });
+});
+
+app.post('/signout', (req, res) => {
+	console.info(`/signout token: ${req.get('Authorization')}`);
+	res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
